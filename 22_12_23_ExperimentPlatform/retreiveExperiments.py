320199c617de086dc247a9a16cecc03c863750d8
antonmerlin <ameganton@gmail.com>
2023-01-16 16:07:52 +0100

    pre-commited retreiveExp.py

--- 22_12_23_ExperimentPlatform/retreiveExperiments.py
+++ 22_12_23_ExperimentPlatform/retreiveExperiments.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
-numberOfAquFunctions=2
-numberOfSurrFunctions=1
-numberOfBenchmarks=2
+numberOfAquFunctions = 2
+numberOfSurrFunctions = 1
+numberOfBenchmarks = 2
 
-dfList=[]
+dfList = []
 
 for aqu in range(numberOfAquFunctions):
     for surr in range(numberOfSurrFunctions):
         for ben in range(numberOfBenchmarks):
-            dfList.append(pd.read_pickle('./results/b'+str(ben)+'a'+str(aqu)+'s'+str(surr)+'.pkl'))
+            dfList.append(
+                pd.read_pickle(
+                    "./results/b" + str(ben) + "a" + str(aqu) + "s" + str(surr) + ".pkl"
+                )
+            )
 
-df=pd.concat(dfList)
-print(df)
+df = pd.concat(dfList)
+# print(df)
 
-df.to_pickle('./concatData.pkl')
+df.to_pickle("./concatData.pkl")
