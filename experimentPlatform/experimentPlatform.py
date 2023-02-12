5ee863b511e32558fedb1908870140c5f05298aa
Meganton <ameganton@gmail.com>
2023-02-13 00:40:38 +0100

    precommit setup

--- experimentPlatform/experimentPlatform.py
+++ experimentPlatform/experimentPlatform.py
@@ -1,6 +1,6 @@
-#from hpobench.benchmarks.ml.nn_benchmark import NNBenchmark
+# from hpobench.benchmarks.ml.nn_benchmark import NNBenchmark
 import os
-import numpy 
+
 import jahs_bench
 import pandas as pd
 from ax import (
@@ -29,11 +29,8 @@ from botorch.acquisition.monte_carlo import (
 from botorch.models.gp_regression import FixedNoiseGP, SingleTaskGP
 
 
-
-
-
-def runExperiment(hydraConfig:dict):
-    #Unpacking dict (from Hydra)
+def runExperiment(hydraConfig: dict):
+    # Unpacking dict (from Hydra)
     numberOfSobolRounds = int(hydraConfig["sobolRounds"])
     numberOfBotorchRounds = int(hydraConfig["botorchRounds"])
     chosenBenchmark = hydraConfig["benchmark"]
@@ -43,31 +40,48 @@ def runExperiment(hydraConfig:dict):
     executeTraining = bool(hydraConfig["executeTraining"])
     saveResults = bool(hydraConfig["saveResults"])
 
-    #Chosing Aquisition and Surrogate Function
+    # Chosing Aquisition and Surrogate Function
     aquisitonFunctions = {
-    "qExpectedImprovement":qExpectedImprovement,
-    "qNoisyExpectedImprovement":qNoisyExpectedImprovement,
-    "ExpectedImprovement":ExpectedImprovement,}
-    surrogateFunctions = {"FixedNoiseGP":FixedNoiseGP, "SingleTaskGP":SingleTaskGP}
+        "qExpectedImprovement": qExpectedImprovement,
+        "qNoisyExpectedImprovement": qNoisyExpectedImprovement,
+        "ExpectedImprovement": ExpectedImprovement,
+    }
+    surrogateFunctions = {"FixedNoiseGP": FixedNoiseGP, "SingleTaskGP": SingleTaskGP}
 
-    #Chosing and initialising Benchmark
+    # Chosing and initialising Benchmark
     if chosenBenchmark == "Branin":
         metric = BraninMetric(name="branin", param_names=["x1", "x2"])
         parameterlist = [
-            RangeParameter(name="x1", parameter_type=ParameterType.FLOAT, lower=-5, upper=10),
-            RangeParameter(name="x2", parameter_type=ParameterType.FLOAT, lower=0, upper=15),
+            RangeParameter(
+                name="x1", parameter_type=ParameterType.FLOAT, lower=-5, upper=10
+            ),
+            RangeParameter(
+                name="x2", parameter_type=ParameterType.FLOAT, lower=0, upper=15
+            ),
         ]
     elif chosenBenchmark == "Hartmann6":
         metric = Hartmann6Metric(
             name="hartmann6", param_names=["x1", "x2", "x3", "x4", "x5", "x6"]
         )
         parameterlist = [
-            RangeParameter(name="x1", parameter_type=ParameterType.FLOAT, lower=0, upper=1),
-            RangeParameter(name="x2", parameter_type=ParameterType.FLOAT, lower=0, upper=1),
-            RangeParameter(name="x3", parameter_type=ParameterType.FLOAT, lower=0, upper=1),
-            RangeParameter(name="x4", parameter_type=ParameterType.FLOAT, lower=0, upper=1),
-            RangeParameter(name="x5", parameter_type=ParameterType.FLOAT, lower=0, upper=1),
-            RangeParameter(name="x6", parameter_type=ParameterType.FLOAT, lower=0, upper=1),
+            RangeParameter(
+                name="x1", parameter_type=ParameterType.FLOAT, lower=0, upper=1
+            ),
+            RangeParameter(
+                name="x2", parameter_type=ParameterType.FLOAT, lower=0, upper=1
+            ),
+            RangeParameter(
+                name="x3", parameter_type=ParameterType.FLOAT, lower=0, upper=1
+            ),
+            RangeParameter(
+                name="x4", parameter_type=ParameterType.FLOAT, lower=0, upper=1
+            ),
+            RangeParameter(
+                name="x5", parameter_type=ParameterType.FLOAT, lower=0, upper=1
+            ),
+            RangeParameter(
+                name="x6", parameter_type=ParameterType.FLOAT, lower=0, upper=1
+            ),
         ]
     elif chosenBenchmark == "Jahs_Bench":
         jahs_benchmark = jahs_bench.Benchmark(task="cifar10")
@@ -84,7 +98,7 @@ def runExperiment(hydraConfig:dict):
                             "arm_name": arm_name,
                             "metric_name": self.name,
                             "trial_index": trial.index,
-                            "mean": 100-results[nepochs]["valid-acc"],
+                            "mean": 100 - results[nepochs]["valid-acc"],
                             "sem": 0.0,
                         }
                     )
@@ -103,7 +117,11 @@ def runExperiment(hydraConfig:dict):
             RangeParameter("LearningRate", ParameterType.FLOAT, 0, 1e-3),
             RangeParameter("WeightDecay", ParameterType.FLOAT, 1e-5, 1e-2),
             ChoiceParameter(
-                "TrivialAugment", ParameterType.BOOL, [True, False], True, sort_values=False
+                "TrivialAugment",
+                ParameterType.BOOL,
+                [True, False],
+                True,
+                sort_values=False,
             ),
             ChoiceParameter(
                 "Op1", ParameterType.INT, [0, 1, 2, 3, 4], False, sort_values=True
@@ -145,8 +163,8 @@ def runExperiment(hydraConfig:dict):
         finally:
             print("No valid benchmark")
 
-    #HPO_Bench_NN_Integration: (uncomment Import to work + create Hydra-File. Problem: getuid()-Command used in PIP/Poetry-Import only works on Linux)
-    '''
+    # HPO_Bench_NN_Integration: (uncomment Import to work + create Hydra-File. Problem: getuid()-Command used in PIP/Poetry-Import only works on Linux)
+    """
     elif chosenBenchmark == "NN_HPO_Bench":
         HPOnnBenchmark=NNBenchmark(task_id=1)
 
@@ -166,7 +184,7 @@ def runExperiment(hydraConfig:dict):
                         }
                     )
                 return Ok(value=Data(df=pd.DataFrame.from_records(records)))
-            
+
         metric=HPOBench_NN_Metric("HPO_NN_metric")
 
         parameterlist=[
@@ -176,59 +194,59 @@ def runExperiment(hydraConfig:dict):
             RangeParameter("learning_rate_init",ParameterType.FLOAT,1e-05,1.0,True),
             RangeParameter("width",ParameterType.INT,16,1024,True)
         ]
-    '''
-    
+    """
 
-    #Creating MockRunner-Class
+    # Creating MockRunner-Class
     class MockRunner(Runner):
         @classmethod
         def run(cls, trial):
             return {"name": str(trial.index)}
 
-    #Initialising Experiment
+    # Initialising Experiment
     exp = Experiment(
-        name="experiment " + chosenAqu+" "+chosenSurr,
+        name="experiment " + chosenAqu + " " + chosenSurr,
         search_space=SearchSpace(parameters=parameterlist),
-        optimization_config=OptimizationConfig(objective=Objective(
+        optimization_config=OptimizationConfig(
+            objective=Objective(
                 metric=metric,
                 minimize=True,
             ),
         ),
-        runner=MockRunner()
+        runner=MockRunner(),
     )
 
     if executeTraining:
-        #Sobol-Trainingrounds
+        # Sobol-Trainingrounds
         sobol = Models.SOBOL(exp.search_space)
-        for i in range(numberOfSobolRounds):
+        for _ in range(numberOfSobolRounds):
             trial = exp.new_trial(generator_run=sobol.gen(1))
             trial.run()
             trial.mark_completed()
 
-        #Botorch-Traningrounds
-        for i in range(numberOfBotorchRounds):
+        # Botorch-Traningrounds
+        for _ in range(numberOfBotorchRounds):
             model_bridge_with_GPEI = Models.BOTORCH_MODULAR(
                 experiment=exp,
                 data=exp.fetch_data(),
                 surrogate=Surrogate(surrogateFunctions[chosenSurr]),
-                botorch_acqf_class=aquisitonFunctions[chosenAqu]
+                botorch_acqf_class=aquisitonFunctions[chosenAqu],
             )
             generator_run = model_bridge_with_GPEI.gen(1)
-            #best_arm, _ = generator_run.best_arm_predictions
+            # best_arm, _ = generator_run.best_arm_predictions
             trial = exp.new_trial(generator_run=generator_run)
             trial.run()
             trial.mark_completed()
 
-        #Data processing
+        # Data processing
         df = exp.fetch_data().df
-        df.insert(0, "surrogate+aquistion", str(chosenSurr+"+"+chosenAqu))
+        df.insert(0, "surrogate+aquistion", str(chosenSurr + "+" + chosenAqu))
         df.insert(0, "benchmark", str(chosenBenchmark))
         df.insert(0, "seed", str(chosenRandomSeed))
         df = df.drop("arm_name", axis="columns")
         df = df.rename(columns={"trial_index": "budget"})
         print(df)
 
-        #Data saving    
+        # Data saving
         if saveResults:
             os.makedirs("./results", exist_ok=True)
             df.to_pickle(
@@ -243,15 +261,16 @@ def runExperiment(hydraConfig:dict):
 
     print("finished")
 
+
 if __name__ == "__main__":
-    testdict={
-        "sobolRounds":"5",
-        "botorchRounds":"15",
-        "benchmark":"Branin",
-        "randomSeed":"0",
-        "aquisitionFunction":"ExpectedImprovement",
-        "surrogateFunction":"SingleTaskGP",
-        "executeTraining":"False",
-        "saveResults":"False"
+    testdict = {
+        "sobolRounds": "5",
+        "botorchRounds": "15",
+        "benchmark": "Branin",
+        "randomSeed": "0",
+        "aquisitionFunction": "ExpectedImprovement",
+        "surrogateFunction": "SingleTaskGP",
+        "executeTraining": "False",
+        "saveResults": "False",
     }
     runExperiment(testdict)
