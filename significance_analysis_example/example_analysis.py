8984ec932af49e2fb8573f75ddfc46f77e4bd81c
Meganton <ameganton@gmail.com>
2023-03-27 15:23:07 +0200

    fixed bugs

--- significance_analysis_example/example_analysis.py
+++ significance_analysis_example/example_analysis.py
@@ -4,9 +4,9 @@ from significance_analysis import conduct_analysis
 
 if __name__ == "__main__":
 
-    data = pd.read_csv("significance_analysis_example/example_dataset.csv")
+    data = pd.read_csv("./significance_analysis_example/example_dataset.csv")
     bin_labels = ["0-16%", "16-36%", "37-57%", "58-79%", "79-100%"]
     bin_dividers = [0.16, 0.36, 0.57, 0.79]
     conduct_analysis(
-        data, "mean", "algorithm", "benchmark", "budget", bin_labels, bin_dividers
+        data, "mean", "acquisition", "benchmark", "budget", bin_labels, bin_dividers
     )
