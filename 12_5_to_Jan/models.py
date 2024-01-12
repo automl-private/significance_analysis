528ca3f00865f90c961ac04698381adbe10822ee
Meganton <ameganton@gmail.com>
2024-01-12 01:17:11 +0100

    Task 1-6 with LMs and without 1|seed

--- 12_5_to_Jan/models.py
+++ 12_5_to_Jan/models.py
@@ -3,7 +3,6 @@ import pickle
 import sys
 
 import matplotlib.pyplot as plt
-import numpy as np
 from sklearn.cluster import KMeans
 import tabulate
 from autorank import autorank, create_report, plot_stats
@@ -11,8 +10,9 @@ from matplotlib.gridspec import GridSpec
 from matplotlib.ticker import MultipleLocator"""
 import typing
 
+import numpy as np
 import pandas as pd
-from pymer4.models import Lmer
+from pymer4.models import Lm, Lmer
 from scipy import stats
 from scipy.stats import rankdata
 
@@ -41,12 +41,42 @@ def combine_bench_prior(row):
     return f"{row['benchmark']}_{row['prior']}"
 
 
-def add_rel_ranks(row, data: pd.DataFrame, benchmark: str):
+def add_rel_ranks(row, data: pd.DataFrame, benchmark: str, time: str):
     values = data.loc[
-        (data[benchmark] == row[benchmark]) & (data["seed"] == row["seed"])
+        (data[benchmark] == row[benchmark])
+        & (data["seed"] == row["seed"])
+        & (data[time] == row[time])
     ]["value"].values
     ranked_data = rankdata(values)
     return ranked_data[values.tolist().index(row["value"])].astype(float)
+
+
+def add_regret(df: pd.DataFrame, normalize: False):
+    best = {}
+    ranges = {}
+    print("⚙️ Preparing regret", end="\r", flush=True)
+    for benchmark in df["bench_prior"].unique():
+        best[benchmark] = min(df.loc[df["bench_prior"] == benchmark]["value"])
+        ranges[benchmark] = (
+            max(df.loc[df["bench_prior"] == benchmark]["value"]) - best[benchmark]
+        )
+
+    def calculate_simple_regret(row, normalize: bool = False):
+        if normalize:
+            return (
+                abs(best[row["bench_prior"]] - row["value"]) / ranges[row["bench_prior"]]
+            )
+        return abs(best[row["bench_prior"]] - row["value"])
+
+    if normalize:
+        print("⚙️ Adding regret       ", end="\r", flush=True)
+        df["regret"] = df.apply(calculate_simple_regret, axis=1, normalize=True)
+        print("✅ Adding regret done                      ")
+    else:
+        print("⚙️ Adding normalized regret       ", end="\r", flush=True)
+        df["norm_regret"] = df.apply(calculate_simple_regret, axis=1, normalize=False)
+        print("✅ Adding normalized regret done                      ")
+    return df
 
 
 def rename_algos(row, algo_dict: dict):
@@ -99,7 +129,98 @@ def create_incumbent(data, fs, f_space, benchmarks, algos, benchmark, algorithm)
     return datasets
 
 
-def glrt(mod1: Lmer, mod2: Lmer, names: list[str] = None) -> dict[str, typing.Any]:
+def create_priorband_benchPrior_relRanks_f24():
+    algorithm = "algorithm"
+    benchmark = "bench_prior"
+    time = "used_fidelity"
+    algos = [
+        "pb_mutation_dynamic_geometric-default-at-target",
+        "random_search",
+        "hyperband",
+    ]
+    fs = [24]
+    f_space = np.linspace(1, max(fs), max(fs)).tolist()
+    benchmarks = [
+        "jahs_cifar10",
+        "jahs_colorectal_histology",
+        "jahs_fashion_mnist",
+        "lcbench-126026",
+        "lcbench-167190",
+        "lcbench-168330",
+        "lcbench-168910",
+        "lcbench-189906",
+        "cifar100_wideresnet_2048",
+        "imagenet_resnet_512",
+        "lm1b_transformer_2048",
+        "translatewmt_xformer_64",
+    ]
+    label_dict = {
+        "random_search": "RS",
+        "hyperband": "HB",
+        "pb_mutation_dynamic_geometric-default-at-target": "PB",
+        "jahs_cifar10": "JAHS-C10",
+        "jahs_colorectal_histology": "JAHS-CH",
+        "jahs_fashion_mnist": "JAHS-FM",
+        "lcbench-126026": "LC-126026",
+        "lcbench-167190": "LC-167190",
+        "lcbench-168330": "LC-168330",
+        "lcbench-168910": "LC-168910",
+        "lcbench-189906": "LC-189906",
+        "cifar100_wideresnet_2048": "PD1-Cifar100",
+        "imagenet_resnet_512": "PD1-ImageNet",
+        "lm1b_transformer_2048": "PD1-LM1B",
+        "translatewmt_xformer_64": "PD1-WMT",
+        "random_search_prior": "RS+Prior",
+        "bo": "BO",
+        "pibo": "PiBO",
+        "bohb": "BOHB",
+        "priorband_bo": "PriorBand+BO",
+    }
+
+    data = load_priorband_data()
+    data = data.loc[
+        (data[algorithm].isin(algos))
+        & (data["benchmark"].isin(benchmarks))
+        & (data["prior"].isin(["at25", "bad"]))
+    ]
+    data["bench_prior"] = data.apply(combine_bench_prior, axis=1)
+    data.drop(columns=["benchmark", "prior"], inplace=True)
+    benchmarks = data[benchmark].unique()
+    max_f = max(fs)
+    data = create_incumbent(data, fs, f_space, benchmarks, algos, benchmark, algorithm)[
+        max_f
+    ]
+    print(f"⚙️ F {max_f}: Adding relative ranks             ", end="\r", flush=True)
+    data["rel_rank"] = data.apply(
+        add_rel_ranks, data=data, benchmark=benchmark, time=time, axis=1
+    )
+    print(f"⚙️ F {max_f}: Renaming algorithms             ", end="\r", flush=True)
+    data[algorithm] = data.apply(rename_algos, algo_dict=label_dict, axis=1)
+    print("✅ Dataset loaded                   ", end="\r", flush=True)
+    return data
+
+
+def add_benchmark_metafeatures(data: pd.DataFrame):
+    meta_feature_df = pd.read_csv("benchmark_metafeatures.csv")
+
+    def add_meta_features(row):
+        return meta_feature_df.loc[
+            meta_feature_df["code_name"] == row["bench_prior"].rsplit("_", 1)[0]
+        ][
+            ["# Vars", "# cont. Vars", "# cond. Vars", "# cat. Vars", "log int", "int"]
+        ].values[
+            0
+        ]
+
+    data[
+        ["n_Vars", "n_cont_Vars", "n_cond_Vars", "n_cat_Vars", "log_int", "int"]
+    ] = data.apply(add_meta_features, axis=1).to_list()
+    return data
+
+
+def glrt(
+    mod1, mod2, names: list[str] = None, returns: bool = False
+) -> dict[str, typing.Any]:
     """Generalized Likelihood Ratio Test on two Liner Mixed Effect Models from R
 
     Args:
@@ -117,29 +238,36 @@ def glrt(mod1: Lmer, mod2: Lmer, names: list[str] = None) -> dict[str, typing.An
     )
     chi_square = 2 * abs(mod1.logLike - mod2.logLike)
     delta_params = abs(len(mod1.coefs) - len(mod2.coefs))
+    p = 1 - stats.chi2.cdf(chi_square, df=delta_params)
     if names:
         print(
-            f"{names[0]} ({round(mod1.logLike,2)}) {'>>' if mod1.logLike>mod2.logLike else '<<' if mod1.logLike<mod2.logLike else '=='} {names[1]} ({round(mod2.logLike,2)})"
+            f"{names[0]} ({round(mod1.logLike,2)}) {'==' if p>0.05 else '>>' if mod1.logLike>mod2.logLike else '<<' if mod1.logLike<mod2.logLike else '=='} {names[1]} ({round(mod2.logLike,2)})"
         )
-        print(
-            f"Chi-Square: {chi_square}, P-Value: {1 - stats.chi2.cdf(chi_square, df=delta_params)}"
+        print(f"Chi-Square: {chi_square}, P-Value: {p}")
+    if returns:
+        return {
+            "p": p,
+            "chi_square": chi_square,
+            "df": delta_params,
+        }
+
+
+def model(formula: str, data: pd.DataFrame, system_id: str = "algorithm"):
+    if "|" in formula:
+        model = Lmer(
+            formula=formula,
+            data=data,
         )
-    return {
-        "p": 1 - stats.chi2.cdf(chi_square, df=delta_params),
-        "chi_square": chi_square,
-        "df": delta_params,
-    }
 
-
-def model(formula: str, system_id: str, data: pd.DataFrame):
-    model = Lmer(
-        formula=formula,
-        data=data,
-    )
-
-    model.fit(
-        factors={system_id: list(data[system_id].unique())},
-        REML=False,
-        summarize=False,
-    )
+        model.fit(
+            factors={system_id: list(data[system_id].unique())},
+            REML=False,
+            summarize=False,
+        )
+    else:
+        model = Lm(
+            formula=formula,
+            data=data,
+        )
+        model.fit(verbose=False, summarize=False)
     return model
